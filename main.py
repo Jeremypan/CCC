98094c23923a1f787a8eda08907b35201b7203db
Hangyu Pan <phyjeremy@outlook.com>
2020-03-26 22:44:26 +1100

    eighth commit

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from mpi4py import MPI
 class ProcessData():
     def __init__(self,args):
         self.args=args
-        self.hashtag=[]
-        self.lang=[]
+        self.hashtag=dict()
+        self.lang=dict()
         self.communicator=MPI.COMM_WORLD
         self.communicator_rank=self.communicator.Get_rank()
         self.communicator_size =self.communicator.Get_size()
@@ -24,28 +24,30 @@ class ProcessData():
                     if line.endswith('[\n') or line.endswith("]\n"):
                         continue
                     elif line.endswith(',\n'):
-                        line = line.replace(line, line[0:len(line) - 2])
+                        line = line[0:len(line) - 2]
                     elif line.endswith('\n'):
-                        line= line.replace(line,line[0:len(line)-1])
+                        line= line[0:len(line)-1]
                     data=json.loads(line)
-                    for hashtag_ele in data['doc']['entities']['hashtags']:
-                        self.hashtag.append(hashtag_ele['text'])
-                    self.lang.append(data['doc']['lang'])
+                    self.statistic_hashtag(data)
+                    self.statistic_lang(data)
                 except json.decoder.JSONDecodeError:
                     return ("Decoder error line: "+line)
-        return self.hashtag,self.lang
+        return sorted(self.hashtag.items(),key=lambda x:x[1],reverse=True),sorted(self.lang.items(),key=lambda x:x[1],reverse=True)
 
-        # jsonstring = ""
-        # for i in read_file.readlines():
-        #     jsonstring = jsonstring + i
-        # try:
-        #     data=json.loads(jsonstring)
-        # except json.decoder.JSONDecodeError:
-        #     if jsonstring[-1] == "\n" and jsonstring[-2] == ",":
-        #         jsonstring = jsonstring[:-2] + "]}"
-        #     data=json.loads(jsonstring)
-        # read_file.close()
-        # return data
+    def statistic_hashtag(self,json_file):
+        for data in json_file['doc']['entities']['hashtags']:
+            hashtag=data['text']
+            if hashtag in list(self.hashtag.keys()):
+                self.hashtag[hashtag]+=1
+            else:
+                self.hashtag[hashtag]=1
+    def statistic_lang(self,json_file):
+        lang=json_file['doc']['lang']
+        if lang in list(self.lang.keys()):
+            self.lang[lang]+=1
+        else:
+            self.lang[lang]=1
+
 
 # def parse_hastag_lang(dict_data):
 
@@ -53,8 +55,12 @@ class ProcessData():
 
 
 if __name__ == "__main__":
+    import time
+    start_time=time.time()
     data_obj=ProcessData(sys.argv[1])
     print(data_obj.read_json())
+    end_time=time.time()
+    print('Time cost: '+str(end_time-start_time)+'s')
     # data=data_obj.read_json()
     # print(data['rows'][0]['doc']['entities'])
 
